ec2481d74eb5ea704629bf12a39154f6eed1d5b5
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-03-01 13:56:03 -0800

    Added starter code for User model and seeds

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: [true, 'Username is required'],
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'Email address is required'],
+            validate: {
+                // return regex.test(v)
+                validator: function(v) {
+                    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+                },
+                message: 'Please enter a valid email address',
+            },
+        },
+        thoughts: [],
+        friends: [],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function() {
+        return `${this.friends.length}`;
+    })
+    .set(function() {
+        const friendCount = this.friends.length;
+        this.set(friendCount);
+    });
+
+const User = model('user', userSchema);
+
+module.exports = User;
--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,25 @@
 const connection = require('../config/connection');
-const { Course, Student } = require('../models');   // change acccording to models
-const { getRandomName, getRandomAssignments } = require('./data');  // change according to models
+const { User } = require('../models');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async() => {
     console.log('connected');
 
-    // Drop existing courses
-    await Course.deleteMany({});
+    // Drop existing users
+    await User.deleteMany({});
 
-    // Drop existing students
-    await Student.deleteMany({});
+    // test seed
+    const user = new User();
+    user.username = 'ryan';
+    user.email = 'ryan@ryan.com';
 
-    // Create empty array to hold the students
-    const students = [];
+    // test validate
+    await user.validate();
 
-    // Loop 20 times -- add students to the students array
-    for (let i = 0; i < 20; i++) {
-        // Get some random assignment objects using a helper function that we imported from ./data
-        const assignments = getRandomAssignments(20);
+    // if valid, insert into collection
+    await User.collection.insertOne(user);
 
-        const fullName = getRandomName();
-        const first = fullName.split(' ')[0];
-        const last = fullName.split(' ')[1];
-        const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
-
-        students.push({
-            first,
-            last,
-            github,
-            assignments,
-        });
-    }
-
-    // Add students to the collection and await the results
-    await Student.collection.insertMany(students);
-
-    // Add courses to the collection and await the results
-    await Course.collection.insertOne({
-        courseName: 'UCLA',
-        inPerson: false,
-        students: [...students],
-    });
-
-    // Log out the seed data to indicate what should appear in the database
-    console.table(students);
     console.info('Seeding complete! 🌱');
     process.exit(0);
 });
