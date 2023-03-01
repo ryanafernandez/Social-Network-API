ffa447cf449b1fed2dda3df043946708bf4a0d2e
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-03-01 15:36:17 -0800

    Added routes for creating a new user, updating a user by id, and deleting a user and their associated thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,8 +21,36 @@ module.exports = {
     },
 
     // POST a new user using username and email
-
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => res.status(500).json(err));
+    },
+    
     // PUT to update a user by its _id
+    updateUser(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+            .then((user) =>
+                !user
+                ?  res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 
     // DELETE to remove a user by its _id. Bonus: remove a user's associated thoughts when deleted.
+    deleteUser(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+            .then((user) => 
+                !user
+                  ?  res.status(404).json({ message: 'No user with that ID' })
+                  : Thought.deleteMany({ _id: { $in: user.thoughts } })
+            )
+            .then(() => res.json({ message: 'User and associated thoughts deleted!' }))
+            .catch((err) => res.status(500).json(err));
+    },
 };
